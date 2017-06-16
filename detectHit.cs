92590184803caa0fdbab48903e9eae7f231d9f0e
catalin209 <zevedei.gheorghe@yahoo.com>
2017-06-16 13:50:56 +0300

    detect_hit

---
+++ detectHit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class detectHit : MonoBehaviour
+{
+
+    public Slider healthbar;
+    Animator anim;
+    public string opponent;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag != opponent) return;
+
+        healthbar.value -= 10;
+
+        if (healthbar.value > 0)
+        {
+
+            anim.SetBool("isReacting", true);
+
+        }
+
+        if (healthbar.value <= 0)
+        {
+            anim.SetBool("isDead", true);
+        }
+
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
